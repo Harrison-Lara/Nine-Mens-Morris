75a05534cd9bfb6a6a66f5f7fab4f3cbf55d6e39
Harrison R. Lara <harrisonlara97@gmail.com>
2019-10-25 16:35:14 -0500

    add tests to node and circle models

--- src/app/gameOver/gameOver.component.spec.ts
+++ src/app/gameOver/gameOver.component.spec.ts
@@ -2,7 +2,7 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { GameOverComponent } from './gameOver.component';
 
-describe('AppComponent', () => {
+describe('Game Over', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -13,9 +13,9 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should Game Over component', async(() => {
+  it('should render Game Over component', async(() => {
     const fixture = TestBed.createComponent(GameOverComponent);
     const comp = fixture.debugElement.componentInstance;
-    expect(comp).toBeFalsy();
+    expect(comp).toBeTruthy();
   }));
 });
