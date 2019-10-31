fd247ad48efa77d36ac343010e292501ad72cd1b
Harrison R. Lara <harrisonlara97@gmail.com>
2019-10-31 17:19:52 -0500

    fix test variables in game service, add Clone method and MoveAllowed tests

--- src/app/service/game.service.spec.ts
+++ src/app/service/game.service.spec.ts
@@ -1,10 +1,7 @@
 import { async } from '@angular/core/testing';
 import { GameService } from './game.service'
 import { Color } from '../model/enum/color.enum';
-import { INode } from '../model/node.model';
 import { MoveType } from '../model/enum/moveType.enum';
-import { IMove } from '../model/move.model';
-import { IPlayerState } from '../model/playerState.model';
 import { HeuristicsType } from '../model/enum/heuristicsType.enum';
 
 describe('Game Service', () => {
@@ -72,20 +69,20 @@ describe('Game Service', () => {
 
   it('should return the Game Service constructor', async(() => {
 
-    const AI = new GameService()
-    expect(AI).toEqual({ "boardCenter": 3 })
+    const constructor = new GameService()
+    expect(constructor).toEqual({ "boardCenter": 3 })
   }));
 
   it('should return the getValueAlmostMill -- End Game', async(() => {
 
-    const AI = new GameService().getValueAlmostMill(mockGameState)
-    expect(AI).toEqual(0)
+    const endGame = new GameService().getValueAlmostMill(mockGameState)
+    expect(endGame).toEqual(0)
   }));
 
   it('should return the getValueAlmostMill -- Draw', async(() => {
 
-    const AI = new GameService().getValueAlmostMill(mockGameStateDraw)
-    expect(AI).toEqual(0)
+    const draw = new GameService().getValueAlmostMill(mockGameStateDraw)
+    expect(draw).toEqual(0)
   }));
 
   it('should return the getValue NAIVE', async(() => {
@@ -100,4 +97,15 @@ describe('Game Service', () => {
     expect(almostMill).toBeCalled
   }));
 
+  it('should return the Clone method', async(() => {
+    const deepClone = { "allowedMoves": [{ "color": "GOLD", "radius": 1, "x": 1, "y": 2 }], "bluePlayerState": { "color": "GOLD", "lastMovedPiece": { "color": "GOLD", "radius": 1, "x": 1, "y": 2 }, "piecesInDrawer": 3, "piecesOnBoard": 4, "points": 1, "previousPosition": { "color": "GOLD", "radius": 1, "x": 1, "y": 2 } }, "chosenForShift": { "color": "GOLD", "radius": 1, "x": 1, "y": 2 }, "goldPlayerState": { "color": "GOLD", "lastMovedPiece": { "color": "GOLD", "radius": 1, "x": 1, "y": 2 }, "piecesInDrawer": 3, "piecesOnBoard": 4, "points": 1, "previousPosition": { "color": "GOLD", "radius": 1, "x": 1, "y": 2 } }, "moveCount": 5, "moveType": "END GAME", "moves": [{ "color": "GOLD", "count": 2, "moveDescription": "normal", "moveType": "NORMAL" }], "movesWithoutMill": 2, "nodes": [{ "color": "GOLD", "radius": 1, "x": 1, "y": 2 }], "shiftDestinations": [{ "color": "GOLD", "radius": 1, "x": 1, "y": 2 }], "turn": "GOLD" }
+    const clone = new GameService().clone(mockGameState)
+    expect(clone).toEqual(deepClone)
+  }));
+
+  it('should return the isMoveAllowed method', async(() => {
+
+    const moveAllowed = new GameService().isMoveAllowed(mockGameState, mockNode)
+    expect(moveAllowed).toEqual(true)
+  }));
 });
