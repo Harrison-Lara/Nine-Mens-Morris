425f874fd1f7878407a9c95f9a04b624ec1263d8
Harrison R. Lara <harrisonlara97@gmail.com>
2019-10-31 20:04:44 -0500

    add tests generateTestDefinitions

--- src/app/model/testDefinition.model.spec.ts
+++ src/app/model/testDefinition.model.spec.ts
@@ -9,4 +9,28 @@ describe('Test Definition Model', () => {
     const testConstructor = new TestDefinition(AlgorithmType.ALPHA_BETA, AlgorithmType.ALPHA_BETA, HeuristicsType.NAIVE, HeuristicsType.NAIVE)
     expect(testConstructor).toEqual({ "blueAiAlgorithm": "ALPHA-BETA", "blueHeuristics": "NAIVE", "goldAiAlgorithm": "ALPHA-BETA", "goldHeuristics": "NAIVE" })
   }))
+
+  it('should return the generateTestDefinitions method -- blueAiAlgorithm ', async(() => {
+
+    const testConstructor = new TestDefinition(AlgorithmType.ALPHA_BETA, AlgorithmType.ALPHA_BETA, HeuristicsType.NAIVE, HeuristicsType.NAIVE).blueAiAlgorithm
+    expect(testConstructor).toEqual("ALPHA-BETA")
+  }))
+
+  it('should return the generateTestDefinitions method -- goldAiAlgorithm  ', async(() => {
+
+    const testConstructor = new TestDefinition(AlgorithmType.MINI_MAX, AlgorithmType.MINI_MAX, HeuristicsType.NAIVE, HeuristicsType.NAIVE).goldAiAlgorithm
+    expect(testConstructor).toEqual("MINI-MAX")
+  }))
+
+  it('should return the generateTestDefinitions method -- blueHeuristics  ', async(() => {
+
+    const testConstructor = new TestDefinition(AlgorithmType.ALPHA_BETA, AlgorithmType.ALPHA_BETA, HeuristicsType.NAIVE, HeuristicsType.NAIVE).blueHeuristics
+    expect(testConstructor).toEqual("NAIVE")
+  }))
+
+  it('should return the generateTestDefinitions method -- goldHeuristics ', async(() => {
+
+    const testConstructor = new TestDefinition(AlgorithmType.ALPHA_BETA, AlgorithmType.ALPHA_BETA, HeuristicsType.NAIVE, HeuristicsType.NAIVE).goldHeuristics
+    expect(testConstructor).toEqual("NAIVE")
+  }))
 });
